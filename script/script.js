601cfc9e16e42c49f79f8a96e55e449c009f244c
DmitriyIvanovIT <strela199720@gmail.com>
2020-09-01 10:17:51 +0300

    feat: Working with modal windows | script/script.js

--- script/script.js
+++ script/script.js
@@ -46,4 +46,73 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     countTimer('01 september 2020');
+
+
+    // Меню
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        let count = 100;
+
+        let idOpenMenu,
+            idCloseMenu;
+
+
+        const openMenu = () => {
+
+                idOpenMenu = requestAnimationFrame(openMenu);
+                if (count > 0) {
+                    menu.style.transform = `translate(-${count -= 10}%)`;
+                } else {
+                    cancelAnimationFrame(idOpenMenu);
+                }
+            },
+            closeMenu = () => {
+                idCloseMenu = requestAnimationFrame(closeMenu);
+                if (count < 100) {
+                    menu.style.transform = `translate(-${count += 10}%)`;
+                } else {
+                    cancelAnimationFrame(idCloseMenu);
+                }
+            },
+            actionMenu = () => {
+                if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+                    if (window.screen.width > 780) {
+                        openMenu();
+                    } else {
+                        menu.style.transform = `translate(0)`;
+                    }
+
+                } else {
+                    if (window.screen.width > 780) {
+                        closeMenu();
+                    } else {
+                        menu.style.transform = `translate(-100%)`;
+                    }
+                }
+            };
+
+        btnMenu.addEventListener('click', actionMenu);
+
+        closeBtn.addEventListener('click', actionMenu);
+
+        menuItems.forEach(item => item.addEventListener('click', actionMenu));
+    };
+
+    toggleMenu();
+
+    // popup
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close');
+
+        popupBtn.forEach(item => item.addEventListener('click', () => popup.style.display = 'block'));
+        popupClose.addEventListener('click', () => popup.style.display = '');
+    };
+
+    togglePopUp();
 });
