069972f527929f4795a7ed5765115f174d3963ca
DmitriyIvanovIT <strela199720@gmail.com>
2020-09-09 11:51:07 +0300

    feat: Creating a validator | script/script.js plugins/validator/validator.js

--- script/script.js
+++ script/script.js
@@ -391,4 +391,72 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     calc(100);
+
+    // eslint-disable-next-line no-undef
+    const validForm1 = new Validator({
+        selector: '#form1',
+        pattern: {},
+        method: {
+            'form1-phone': [
+                ['notEmpty'],
+                ['pattern', 'phone']
+            ],
+            'form1-email': [
+                ['notEmpty'],
+                ['pattern', 'email']
+            ],
+            'form1-name': [
+                ['notEmpty'],
+                ['pattern', 'text']
+            ]
+        }
+    });
+
+    validForm1.init();
+
+    const validForm2 = new Validator({
+        selector: '#form2',
+        pattern: {},
+        method: {
+            'form2-phone': [
+                ['notEmpty'],
+                ['pattern', 'phone']
+            ],
+            'form2-email': [
+                ['notEmpty'],
+                ['pattern', 'email']
+            ],
+            'form2-name': [
+                ['notEmpty'],
+                ['pattern', 'text']
+            ],
+            'form2-message': [
+                ['notEmpty'],
+                ['pattern', 'text']
+            ]
+        }
+    });
+
+    validForm2.init();
+
+    const validForm3 = new Validator({
+        selector: '#form3',
+        pattern: {},
+        method: {
+            'form3-phone': [
+                ['notEmpty'],
+                ['pattern', 'phone']
+            ],
+            'form3-email': [
+                ['notEmpty'],
+                ['pattern', 'email']
+            ],
+            'form3-name': [
+                ['notEmpty'],
+                ['pattern', 'text']
+            ]
+        }
+    });
+
+    validForm3.init();
 });
